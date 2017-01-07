7b076b8038233bad4329e166a07b78f6ebc758e8
Ian <ianw3214@gmail.com>
2017-01-07 18:32:30 -0500

    Implement image process

--- web/index.js
+++ web/index.js
@@ -1,4 +1,26 @@
+function upload(){
+  document.getElementById("input").click();
+}
+
 function test(){
-  document.getElementById('block2').style.display = "none";
-  console.log("CHEESE");
+  // send the image to the server when the user inputs a file
+  image_to_base64("input", function(base64){
+
+    // process the server response and display it
+    food = new MicrowaveFood(base64, function(food){
+
+      new_html="";
+
+      if(food !== false){
+        new_html = "Microwave Time: " + food.how_long().toString() + "\n" +
+              "Cal: " + food.how_much_cal().toString());
+      } else {
+        new_html = "Failed to upload image";
+      }
+
+      document.getElementById("block3").innerHTML = new_html;
+
+    });
+
+  });
 }
