8851074f44fd35108f23e04738c85518e7924706
Jun Zheng <junzheng@exchange.niyume.com>
2017-01-08 04:44:45 -0500

    Style update

--- web/index.js
+++ web/index.js
@@ -21,13 +21,13 @@ function change(){
                         if(data.warning){
                             $("#warning-container").fadeIn();
                         }
-                        new_html = "<img src='../api/images/" + food.img_url + "' /><br><br>";
+                        new_html = "<span class='mic-time'>Suggested Microwave Time: " + data.total_cook_time + " Seconds.</span><br>" + "Appox. Calories: " + data.total_cal + "<br><br>";
+                        new_html += "<img src='../api/images/" + food.img_url + "' /><br><br>";
                         new_html += "Based on our sophisticated scientific algorithm, your dish may contain ...<br>";
                         for (i=0; i<data.score.length; i++){
                             new_html += "<span class='label label-default'>" + data.score[i][0] + "</span>"
                         }
-                        new_html += "<br><br><span class='mic-time'>Suggested Microwave Time: " + data.total_cook_time + " Seconds.</span><br>" + "Appox. Calories: " + data.total_cal + "<br><br>";
-                        new_html += "<div class='row'><div class='col-md-4'><button onclick='feedback();' class='vote-button'><i class='fa fa-snowflake-o' aria-hidden='true'></i> Too Cold</button></div>"
+                        new_html += "<br><br><div class='row'><div class='col-md-4'><button onclick='feedback();' class='vote-button'><i class='fa fa-snowflake-o' aria-hidden='true'></i> Too Cold</button></div>"
                         new_html += "<div class='col-md-4'><button onclick='feedback();' class='vote-button'><i class='fa fa-thumbs-up' aria-hidden='true'></i> Perfact</button></div>"
                         new_html += "<div class='col-md-4'><button onclick='feedback();' class='vote-button'><i class='fa fa-fire' aria-hidden='true'></i> Too Hot</button></div></div>"
                         document.getElementById("block3").innerHTML = new_html;
