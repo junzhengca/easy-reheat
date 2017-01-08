6ef31b4323f1af0d25b8acd233c92e7091dd53b6
Jun Zheng <junzheng@exchange.niyume.com>
2017-01-08 01:21:34 -0500

    Feedback button

--- web/index.js
+++ web/index.js
@@ -24,9 +24,9 @@ function change(){
                             new_html += "<span class='label label-default'>" + data.score[i][0] + "</span>"
                         }
                         new_html += "<br><br><span class='mic-time'>Microwave Time: " + data.total_cook_time + " Seconds.</span><br>" + "Calories: " + data.total_cal + "<br><br>";
-                        new_html += "<div class='row'><div class='col-md-4'><button class='vote-button'>Too Cold</button></div>"
-                        new_html += "<div class='col-md-4'><button class='vote-button'>Perfact</button></div>"
-                        new_html += "<div class='col-md-4'><button class='vote-button'>Too Hot</button></div></div>"
+                        new_html += "<div class='row'><div class='col-md-4'><button onclick='feedback();' class='vote-button'>Too Cold</button></div>"
+                        new_html += "<div class='col-md-4'><button onclick='feedback();' class='vote-button'>Perfact</button></div>"
+                        new_html += "<div class='col-md-4'><button onclick='feedback();' class='vote-button'>Too Hot</button></div></div>"
                         document.getElementById("block3").innerHTML = new_html;
                     });
                 } else {
@@ -38,6 +38,12 @@ function change(){
         });
     }, 500);
 }
+
+function feedback(){
+    alert("Thanks for your feedback!");
+    window.location.reload();
+}
+
 
 $(function(){
     $(".vote-button").click(function(){
